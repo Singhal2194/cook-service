31ec3db8b6fcf5bd3d52c4550ae7974640f1e33d
satish.agarwal <satish.agarwal@olacabs.com>
2020-05-03 16:28:43 +0530

    response parse

--- src/main/java/com/cook/CookServer/app/Constants.java
+++ src/main/java/com/cook/CookServer/app/Constants.java
@@ -6,7 +6,8 @@ public class Constants {
     public static final String STATUS_FAILURE = "failure";
     public static final String STATUS_SUCCESS = "success";
     public static final String REASON = "reason";
-    public static final String NUMBER_NOT_REGISTERED = "NUMBER_NOT_REGISTERED";
+    public static final String NUMBER_NOT_REGISTERED = "This number is not registered.Please get your number registered";
     public static final String COOKID = "cook_id";
     public static final String COOKID_NOT_FOUND = "COOKID_NOT_FOUND";
+    public static final String MESSAGE = "message";
 }
